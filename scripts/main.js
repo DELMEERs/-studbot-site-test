e177c31ca0b3c669528f2cf7f9a25883ddb1f5d6
DELMEERs <morogindima@yandex.ru>
2024-12-18 18:55:47 +0700

    14

--- scripts/main.js
+++ scripts/main.js
@@ -2,20 +2,6 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-// document.getElementsByClassName("s-btn")[0].addEventListener("click", () => {
-//     let title = document.getElementsByClassName("title-inp")[0];
-//     let description = document.getElementsByClassName("desc-inp")[0];
-//     let text = document.getElementsByClassName("text-inp")[0];
-
-//     let data = {
-//         title: title.value,
-//         desc: description.value,
-//         text: text.value
-//     }
-
-//     tg.sendData(JSON.stringify(data));
-// });
-
 const changeTextButton = document.getElementById('changeTextButton');
 const textToChange = document.getElementById('textToChange');
 
