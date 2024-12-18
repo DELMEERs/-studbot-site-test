3d14b08fdcfbdc8ac730e1db4e9880d1d98d21b4
DELMEERs <morogindima@yandex.ru>
2024-12-18 18:48:58 +0700

    12

--- scripts/main.js
+++ scripts/main.js
@@ -2,27 +2,23 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-document.getElementsByClassName("s-btn")[0].addEventListener("click", () => {
-    let title = document.getElementsByClassName("title-inp")[0];
-    let description = document.getElementsByClassName("desc-inp")[0];
-    let text = document.getElementsByClassName("text-inp")[0];
+// document.getElementsByClassName("s-btn")[0].addEventListener("click", () => {
+//     let title = document.getElementsByClassName("title-inp")[0];
+//     let description = document.getElementsByClassName("desc-inp")[0];
+//     let text = document.getElementsByClassName("text-inp")[0];
 
-    let data = {
-        title: title.value,
-        desc: description.value,
-        text: text.value
-    }
+//     let data = {
+//         title: title.value,
+//         desc: description.value,
+//         text: text.value
+//     }
 
-    tg.sendData(JSON.stringify(data));
-});
+//     tg.sendData(JSON.stringify(data));
+// });
 
 const changeTextButton = document.getElementById('changeTextButton');
 const textToChange = document.getElementById('textToChange');
 
 changeTextButton.addEventListener('click', () => {
-    console.log("tg.initData");
-    textToChange.innerText = tg.initData;
-
-    console.log("tg.initData.first_name");
-    textToChange.innerText = tg.initData.first_name;
+    textToChange.innerText = tg.initData.id;
 });
