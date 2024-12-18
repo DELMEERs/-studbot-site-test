d4fcce7dc2b574c5ae1adc4fb6105eab5c9bf5fd
DELMEERs <morogindima@yandex.ru>
2024-12-18 16:25:56 +0700

    5

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,19 @@
 // "use strict";
 let tg = window.Telegram.WebApp;
+
 tg.expand();
 
+document.getElementsByClassName("s-btn")[0].addEventListener("click", () => {
+    let title = document.getElementsByClassName("title-inp")[0];
+    let description = document.getElementsByClassName("desc-inp")[0];
+    let text = document.getElementsByClassName("text-inp")[0];
+
+    let data = {
+        title: title.value,
+        desc: description.value,
+        text: text.value
+    }
+
+    tg.sendData(JSON.stringify(data));
+});
 // alert('Привет, мир!');
